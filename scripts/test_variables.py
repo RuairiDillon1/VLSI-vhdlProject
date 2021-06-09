278c75827a6e1959c321faaf4674b27d9bdb2f57
Ruairí <ruairi.dillon@hs-augsburg.de>
2021-06-09 14:52:33 +0200

    updated the dictionary with addresses etc

--- scripts/test_variables.py
+++ scripts/test_variables.py
@@ -1,5 +1,17 @@
 # name of function | address byte | data byte
 serial_vars = {
-    "enable_pm" : bytearray.fromhex("0a ff"), # example only bytes not correct
-
+    "system_control" : bytearray.fromhex("01 03"), 
+    
+    "pwm_pulse_width" : bytearray.fromhex("04 ff"),
+    "pwm_period" : bytearray.fromhex("05 ff"),
+    "pwm_control" : bytearray.fromhex("04 03"),
+    
+    "noise_prbsg_length" : bytearray.fromhex("08 ff"),
+    "noise_period" : bytearray.fromhex("09 ff"),
+    "noise_control" : bytearray.fromhex("0b 03"),
+    
+    "pattern_length" : bytearray.fromhex("0c ff"),
+    "pattern_data" : bytearray.fromhex("0d ff"),
+    "pattern_period" : bytearray.fromhex("0e ff"),
+    "pattern_control" : bytearray.fromhex("0f 07"),
 }
