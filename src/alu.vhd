a00b8a5922d1fd05155ba6b8de8ecd1dbbde8616
Ruairí <ruairi.dillon@hs-augsburg.de>
2021-06-15 20:15:10 +0200

    basic comments added to alu file

--- src/alu.vhd
+++ src/alu.vhd
@@ -1,10 +1,15 @@
 -------------------------------------------------------------------------------
 -- Module     : ALU
 -------------------------------------------------------------------------------
--- Author     : Johann Faerber
+-- Author     : Hillinger, Dillon & Cunningham
 -- Company    : University of Applied Sciences Augsburg
 -------------------------------------------------------------------------------
--- Description: 
+-- Description: This module offers the use the ability to perform logical
+-- arithmetic on two three bit signals. It also offers the user the ability to
+-- select an operation via the sel_i variable.
+-- In the scope of the project, the output is meant to be sent to LEDRs. The
+-- pattern generator will be connected (first three wired to a_i, next three to
+-- b_i and the final two to be used for the select.
 -------------------------------------------------------------------------------
 -- Revisions  : see end of file
 -------------------------------------------------------------------------------
@@ -23,19 +28,20 @@ END ENTITY alu;
 
 ARCHITECTURE rtl OF alu IS
 
- SIGNAL y_out  : unsigned(2 DOWNTO 0);
+ SIGNAL y_out  : unsigned(2 DOWNTO 0); -- set as unsigned so it can be written
+                                       -- to after the conversion
   
 BEGIN
 
-  WITH sel_i SELECT y_out <=
-    (unsigned(a_i) + unsigned(b_i)) WHEN "00",
-    (unsigned(a_i) - unsigned(b_i)) WHEN "01",
+  WITH sel_i SELECT y_out <= -- uses select value to determine the operation below
+    (unsigned(a_i) + unsigned(b_i)) WHEN "00", -- converted to unsigned to makelogical arithmetic possible
+    (unsigned(a_i) - unsigned(b_i)) WHEN "01", -- WHEN statement
     (unsigned(a_i and b_i)) WHEN "10", -- possible issues with data type
     (unsigned(a_i or b_i)) WHEN "11", -- possible issues with data type
     (OTHERS => '0') WHEN others;
 
-  y_o <= std_ulogic_vector(y_out);
-  
+  y_o <= std_ulogic_vector(y_out); -- converting the output to std_ulogic_vect
+                                   -- to output
 
 END rtl;
 
